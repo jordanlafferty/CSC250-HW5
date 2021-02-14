31597a22a710c9b02794f2d8905b6a689bd27309
Jordan Lafferty <jordanlafferty99@yahoo.com>
2021-02-14 00:02:58 -0600

    HW 5

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,16 @@
 
 public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+		soccerplayers p1 = new soccerplayers("Lionel Messi", "Argentina", "Barcelona", " Center Forward", 10);
+		soccerplayers p2 = new soccerplayers("Becky Sauerbrunn", "United States", "Portland Thorns", "Center Back", 4);
+		soccerplayers p3 = new soccerplayers("Rose Lavelle", "United States", "Manchester City", " Central Midfielder", 21);
+		soccerplayers p4 = new soccerplayers("Fabio Tavares", "Brazil", "Liverpool", "Right Back", 3);
+		p1.display();
+		p2.display();
+		p3.display();
+		p4.display();
 	}
 
 }
--- src/soccerplayers.java
+++ src/soccerplayers.java
@@ -1,4 +1,24 @@
 
 public class soccerplayers {
+	private String name;
+	private String position;
+	private String nteam;
+	private String cteam;
+	private int number;
+	
+	public soccerplayers(String name, String nteam, String cteam, String position, int number) 
+	{
+		this.name = name;
+		this.nteam = nteam;
+		this.cteam = cteam;
+		this.position = position;
+		this.number = number;
+		
+	}
+	
+	void display()
+	{
+		System.out.println("Name: " + this.name + "\nNational Team: " + this.nteam + "\nClub Team: " + this.cteam + "\nPosition: " +  this.position + "\nJersey Name: " + this.number);
+	}
 
 }
